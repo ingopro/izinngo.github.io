7acefff3f1b58edc1fbc82bc5f61ba6c93196a23
Jerry <hwy0127@gmail.com>
2019-11-14 20:55:44 +0800

    'update'

---
+++ scripts/Autoopen.js
@@ -0,0 +1,6 @@
+﻿var spawn = require('child_process').exec;
+
+// Hexo 3 用户复制这段
+hexo.on('new', function(data){
+  spawn('start "" "C:\\Users\\Jerry\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe" ' + data.path);
+});
